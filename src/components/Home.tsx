eb210ff094a990767865d1a35b30a0d1d08ea7cc
fraylin <fraylinayala321@gmail.com>
2026-01-11 17:22:52 -0500

    Added logic for cook button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,36 +1,59 @@
-import {useState} from "react"
+import { useState } from "react"
 import "../assets/Home.css"
+import axios from "axios"
 
-function Home(){
-    const [type, setType] = useState([false,false])
+function Home() {
+    const [type, setType] = useState([false, false])
     const [region, setRegion] = useState("")
     const [time, setTime] = useState("")
 
-    const handleType = (index: number) =>{
-        setType((prev)=> {
-            return prev.map((_,i) => i === index)
+    const handleType = (index: number) => {
+        setType((prev) => {
+            return prev.map((_, i) => i === index)
         })
     }
 
-    const handleRegion = (event : React.ChangeEvent<HTMLSelectElement>) =>{
+    const handleRegion = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setRegion(event.target.value)
     }
 
-    const handleTime = (event : React.ChangeEvent<HTMLSelectElement>) =>{
+    const handleTime = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setTime(event.target.value)
     }
 
-    return(
+    const generateRecipe = async () => {
+        if ((!type[0] && !type[1]) || !region || !time) {
+            alert("Missing input. Make sure there are no blanks")
+            return;
+        }
+        try {
+            const response = await fetch("http://localhost:5173/api/exercises", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({ type, region, time })
+
+            });
+
+            const data = await response.json();
+            console.log("Recipe")
+        } catch (error){
+            console.log("Error found when generating recipe:", error)
+        }
+    }
+
+    return (
         <div className="main">
             <h1>Recipe App</h1>
             <h3>What type of dish are you feeling?</h3>
             <div className="foodButtons">
-                <button onClick = {() => handleType(0)} style={{backgroundColor: type[0] ? "gray": "rgb(43, 39, 39)"}}>Sweet</button>
-                <button onClick = {() => handleType(1)} style={{backgroundColor: type[1] ? "gray": "rgb(43, 39, 39)"}}>Salty</button>
+                <button onClick={() => handleType(0)} style={{ backgroundColor: type[0] ? "gray" : "rgb(43, 39, 39)" }}>Sweet</button>
+                <button onClick={() => handleType(1)} style={{ backgroundColor: type[1] ? "gray" : "rgb(43, 39, 39)" }}>Salty</button>
             </div>
 
             <h3>From what region?</h3>
-            <select id="regions" name="regions" value = {region} onChange={handleRegion}>
+            <select id="regions" name="regions" value={region} onChange={handleRegion}>
                 <option value=""></option>
                 <option value="africa">Africa</option>
                 <option value="americas">Americas</option>
@@ -43,18 +66,18 @@ function Home(){
 
             <h3>Cooking time?</h3>
             <select id="time" name="time" value={time} onChange={handleTime}>
-                <option value = ""></option>
+                <option value=""></option>
                 <option value="30 minutes">30 minutes</option>
                 <option value="1 hour">1 hour</option>
                 <option value="2 hour">2 hours</option>
             </select>
-            
-            <button id="cookButton">Cook</button>
+
+            <button id="cookButton" onClick={generateRecipe}>Cook</button>
 
             <div className="generatedContent">
 
             </div>
-        
+
         </div>
     )
 
